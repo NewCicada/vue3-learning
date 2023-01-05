106bc89d30354b61af8ca2d0fd23a3e7fca74c61
NewCicada <1547598968@qq.com>
2023-01-05 22:33:29 +0800

    08_src_watch监视函数

---
+++ 07_src_vue3的计算属性/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Demo @hello="showHelloMsg" msg="你好啊" school="尚硅谷">
+    <template v-slot:qwe>
+      <span>尚硅谷</span>
+    </template>
+    <template v-slot:asd>
+      <span>尚硅谷</span>
+    </template>
+  </Demo>
+</template>
+
+<script>
+import Demo from './components/demo.vue'
+export default {
+  name: 'App',
+  components: {
+    Demo,
+  },
+}
+</script>
+
+<style>
+</style>
