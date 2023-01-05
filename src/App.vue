eeef9c8a897c671d64a7f719f645eef653569e83
NewCicada <1547598968@qq.com>
2023-01-05 11:05:03 +0800

    reactive函数

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,47 @@
 <template>
   <h1>一个人的信息</h1>
-  <h2>姓名:{{ name }}</h2>
-  <h2>年龄:{{ age }}</h2>
-  <h3>工作种类:{{ job.type }}</h3>
-  <h3>薪资:{{ job.salary }}</h3>
+  <h2>姓名:{{ person.name }}</h2>
+  <h2>年龄:{{ person.age }}</h2>
+  <h3>工作种类:{{ person.job.type }}</h3>
+  <h3>工作薪资:{{ person.job.salary }}</h3>
+  <h3>测试的数据c:{{ person.job.a.b.c }}</h3>
+  <h3>爱好:{{ person.hobby }}</h3>
   <button @click="changeInfo">修改人的信息</button>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { reactive } from 'vue'
 export default {
   name: 'App',
+  // 数据
   setup() {
-    // 数据
-    let name = ref('张三')
-    let age = ref(18)
-    let job = ref({
-      type: '前端工程师',
-      salary: '30k',
+    let person = reactive({
+      name: '张三',
+      age: 18,
+      job: {
+        type: '前端工程师',
+        salary: '30k',
+        a: {
+          b: {
+            c: 666,
+          },
+        },
+      },
+      hobby: ['抽烟', '喝酒', '烫头'],
     })
     // 方法
     function changeInfo() {
-      name.value = '李四'
-      age.value = 48
-      // console.log(job.value) 
-      job.value.type = 'UI设计师'
-      job.value.salary = '80k'
-      // console.log(name, age)
+      person.name = '李四'
+      person.age = 48
+      person.job.type = 'UI设计师'
+      person.job.salary = '80k'
+      person.job.a.b.c = 999
+      person.hobby[0] = '学习'
     }
-    // 返回一个对象
+    // 返回一个对象(常用)
     return {
-      name,
-      age,
+      person,
       changeInfo,
-      job,
     }
   },
 }
