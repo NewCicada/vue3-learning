31724a27b03f6f26bab2a9fb709ae192a2e75189
NewCicada <1547598968@qq.com>
2023-01-06 21:04:37 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
 <template>
-  <button @click="isShowDemo != isShowDemo">切换隐藏/显示</button>
-  <demo v-if="isShowDemo" />
+  <input type="text" v-model="keyWord" />
+  <h3>{{ keyWord }}</h3>
 </template>
 
 <script>
-import { ref } from 'vue'
-import Demo from './components/Demo.vue'
+import { ref, customRef } from 'vue'
 export default {
   name: 'App',
-  components: {
-    Demo,
-  },
-
   setup() {
-    let isShowDemo = ref(true)
-    return {
-      isShowDemo,
+    // 自定义一个ref--名为myRef
+    function myRef(value, delay) {
+      let timer
+      // 通过customRef去实现自定义
+      return customRef((track, trigger) => {
+        return {
+          get() {
+            console.log(`有人从myRef这个容器中读取数据了,我把${value}给他了`)
+            track() //告诉Vue这个value值是需要被“追踪”的
+            return value
+          },
+          set(newValue) {
+            console.log(`有人把myRef这个容器中数据改为了${newvalue}`)
+            // eslint-disable-next-line no-undef
+            clearTimeout(timer)
+            // eslint-disable-next-line no-undef
+            timer = setTimeout(() => {
+              value = newValue
+              trigger() //通知Vue去更新界面
+            }, delay)
+          },
+        }
+      })
     }
+    // let keyWord = ref(hello)//使用Vue提供的ref
+    let keyWord = myRef('hello', 500) //使用程序员自定义的ref
+
+    return { keyWord }
   },
 }
 </script>
