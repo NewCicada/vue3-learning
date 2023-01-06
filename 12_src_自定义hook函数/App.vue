186f81bb3ed4d04f80c27c07badabb9d4edcfd8b
NewCicada <1547598968@qq.com>
2023-01-06 13:46:26 +0800

    自定义hook函数

---
+++ 12_src_自定义hook函数/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="isShowDemo != isShowDemo">切换隐藏/显示</button>
+  <demo v-if="isShowDemo" />
+  <hr />
+  <Test />
+</template>
+
+<script>
+import { ref } from 'vue'
+import Demo from './components/Demo.vue'
+import Test from './components/Test.vue'
+export default {
+  name: 'App',
+  components: {
+    Demo,
+    Test,
+  },
+
+  setup() {
+    let isShowDemo = ref(true)
+    return {
+      isShowDemo,
+    }
+  },
+}
+</script>
+
+<style>
+</style>
